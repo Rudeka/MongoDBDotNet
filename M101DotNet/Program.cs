9b4b375b82c2ce65f90dceb0b20c074ba5fde6eb
Ihor Rudenko <irudenko@softserveinc.com>
2017-11-02 21:58:59 +0200

    Add Poco mapping

---
+++ M101DotNet/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace M101DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var conventionPack = new ConventionPack();
+            conventionPack.Add(new CamelCaseElementNameConvention());
+            ConventionRegistry.Register("camelCase", conventionPack, t => true);
+            //BsonClassMap.RegisterClassMap<Person>(cm =>
+            //{
+            //    cm.AutoMap();
+            //    cm.MapMember(x => x.Name).SetElementName("Name");
+            //});
+            var person = new Person
+            {
+                Name = "Jones",
+                Age = 30,
+                Colors = new List<string>{ "red", "blue" },
+                Pets = new List<Pet> { new Pet { Name = "Fluffy", Type = "Pig"} },
+                ExtraElements = new BsonDocument("anotherName", "anotherValue")
+            };
+
+            using (var writer = new JsonWriter(Console.Out))
+            {
+                BsonSerializer.Serialize(writer, person);
+            }
+        }
+    }
+
+    
+
+    class Person
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Colors { get; set; }
+        public List<Pet> Pets { get; set; }
+        public BsonDocument ExtraElements { get; set; }
+    }
+
+    class Pet
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
